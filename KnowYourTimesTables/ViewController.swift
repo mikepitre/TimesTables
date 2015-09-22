0c78c98d6a95dd0016eab42b01b513ff7a1895a1
mikepitre <michaelepitre@gmail.com>
2015-09-22 12:06:48 -0500

    used tables views and slider to create times tables app

---
+++ KnowYourTimesTables/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  KnowYourTimesTables
+//
+//  Created by Mike Pitre on 9/22/15.
+//  Copyright © 2015 Mike Pitre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet weak var table: UITableView!
+    
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+        
+        table.reloadData()
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 20
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        let timesTable = Int(sliderValue.value)
+        
+        cell.textLabel?.text = String(timesTable * (indexPath.row + 1))
+        
+        return cell
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
